5ab7cf257cd6181c5d806aeaf6da042f157074df
kanishka viraj <kanishkaviraj12@gmail.com>
2023-11-15 23:48:36 +0530

    db connection

---
+++ conection.php
@@ -0,0 +1,11 @@
+<?php 
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "food";  
+    $conn =  mysqli_connect($servername, $username, $password, $db_name);
+    if($conn->connect_error){
+        die("Connection failed".$conn->connect_error);
+    }
+    echo "Connected successfully";
+?>
